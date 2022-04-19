1d762022b0ed5db07a2960ba78d865e6b660107d
bharathrnair <bharathrnair7@gmail.com>
2022-04-19 11:36:13 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,21 @@ function App() {
     <div className="mainContainer">
 
       <div className="loginContainer">
-
+        <div className="loginSection">
+        <input type="text" placeholder='username' id='username' />
+        <br />
+        <input type="text" placeholder='password'  id='password' />
+        </div>
+      
+      <div className="loginButton">
+        <button id='login'>Log in</button>
       </div>
+        <p>"connect people around"</p>
+        </div>
+      
       <div className="signUpContainer">
+
+        
 
       </div>
 
