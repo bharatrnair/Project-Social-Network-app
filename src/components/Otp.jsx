619f09d87ee22c69c3f3989346d4aeff2b6d17a7
bharathrnair <bharathrnair7@gmail.com>
2022-04-20 13:36:58 +0530

    commit

--- src/components/Otp.jsx
+++ src/components/Otp.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import next from './../components/next.png'
+
 
 const Otp = () => {
   return (
@@ -7,11 +9,20 @@ const Otp = () => {
             <div className="otpDetailsSection">
                 <p id='otp'>ENTER OTP</p>
                 <div className="otpInput">
-                    <input className='underline' type="text" />
-                    <input className='underline' type="text" />
-                    <input className='underline' type="text" />
-                    <input className='underline' type="text" />
+                    <input className='underline' type="text" maxLength={1} />
+                    <input className='underline' type="text" maxLength={1}/>
+                    <input className='underline' type="text" maxLength={1}/>
+                    <input className='underline' type="text" maxLength={1}/>
             </div>
+            <div className="messageSection">
+                <p>Resend OTP</p>
+            </div>
+            <div className="nextButton">
+                <img src={next} alt="" />
+
+            </div>
+
+
 
         </div>
         </div>
