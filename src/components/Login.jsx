894cda5e5d43fbcd3646161c94a901b390b8d252
bharathrnair <bharathrnair7@gmail.com>
2022-04-21 14:39:40 +0530

    commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
         </div>
         </div>
       
-      <div className= "signUpContainer">
+        <div className= "signUpContainer">
         <div className="signUpSection">
         <p id='two'>Sign Up</p>
         <div className="signUpInput">
