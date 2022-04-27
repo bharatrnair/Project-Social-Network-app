3143d0ae480e83c9840d0466894839653e31e8e6
bharathrnair <bharathrnair7@gmail.com>
2022-04-27 11:23:37 +0530

    commit

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import back from './../components/Assets/back.svg'
+import cam from './../components/Assets/cam.svg'
 
 const Chat = () => {
   return (
     <div className="mainChat">
-        <div className="backgroundChat">
-
+        <div className="chatBody">
+        <div className="chatHeader">
+        <img className='backButton' src={back} alt="" />
+        <div className="searchText">
+        <input type="text" id='inp' placeholder='search...'/>
         </div>
+        </div>
+        <div className="chats">
+          <div className="userDp"></div>
+           <div className="userName"><h2>User1</h2></div>
+           <div className="camSection"> <img src={cam}alt="" /></div>
+           <div className="userDp"></div>
+           <div className="userName"><h2>User2</h2></div>
+           <div className="camSection"> <img src={cam}alt="" /></div>
+           <div className="userDp"></div>
+           <div className="userName"><h2>User3</h2></div>
+           <div className="camSection"> <img src={cam}alt="" /></div>
+           <div className="userDp"></div>
+           <div className="userName"><h2>User4</h2></div>
+           <div className="camSection"> <img src={cam}alt="" /></div>
+           <div className="userDp"></div>
+           <div className="userName"><h2>User5</h2></div>
+           <div className="camSection"> <img src={cam}alt="" /></div>
+        </div>
+
+    </div>
     </div>
   )
 }
