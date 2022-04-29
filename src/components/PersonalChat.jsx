f71a967c226de5594913290d7a77c1a8a31963f6
bharathrnair <bharathrnair7@gmail.com>
2022-04-29 12:28:16 +0530

    commit

--- src/components/PersonalChat.jsx
+++ src/components/PersonalChat.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const PersonalChat = () => {
   return (
-      <div className="Personal-chat-body">
-          
+      <div className="Personal-chat-main">
+          <div className="Personal-chat-body">
+              <div className="Personal-chat-header">
+                  <div className="Personal-chat-dp"></div>
+                  <div className="Personal-chat-user"><h2>User</h2></div>
+              </div>
+          </div>
+
       </div>
 
   )
