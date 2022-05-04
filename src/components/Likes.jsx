a6188ba42f3f4f83513fc79caf578b84fac7ae38
bharathrnair <bharathrnair7@gmail.com>
2022-05-04 12:31:14 +0530

    commit

--- src/components/Likes.jsx
+++ src/components/Likes.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import like from './../components/Assets/Vectorlike.svg'
 
 const Likes = () => {
   return (
     <div className="Like-mainContainer">
         <div className="likeBody">
         <div className="likeHeader">
+         <p id='likeHeading'>likes</p>
+         <img src={like} alt="" />
+        </div>
+        <div className="users-liked">
+            <div className="users-who-liked">
+                <div className="likedUsersDp">
 
+                </div>
+                <div className="likeUserName">
+                    <p>user 1</p>
+                </div>
+            </div>
         </div>
         </div>
     </div>
