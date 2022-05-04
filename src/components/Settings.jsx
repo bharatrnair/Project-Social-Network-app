9993e43411d5838f186d1cd487f55e2d5a21a62b
bharathrnair <bharathrnair7@gmail.com>
2022-05-04 10:05:24 +0530

    commit

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -9,19 +9,19 @@ const Settings = () => {
               </div>
               <div className="settings-section">
                   <div className="settings-property">
-                 
+                   <h3>Edit Profile</h3>
                   </div>
                   <div className="settings-property">
-                      
+                      <h3>Privacy & Security</h3>
                       </div>
                       <div className="settings-property">
-                      
+                      <h3>Blocked Accounts</h3>
                       </div>
                       <div className="settings-property">
-                      
+                      <h3>Privacy Policy & Community Guidelines</h3>
                       </div>
                       <div className="settings-property">
-                      
+                      <h3>Log Out</h3>
                       </div>
               </div>
           </div>
