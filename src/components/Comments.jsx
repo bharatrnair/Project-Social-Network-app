32dd06c736708be5ab0f74f2627b80256e5a1be8
bharathrnair <bharathrnair7@gmail.com>
2022-05-04 10:25:43 +0530

    commit

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import comments from './../components/Assets/comments.svg'
 
 const Comments = () => {
   return (
     <div className="comments-mainContainer">
-        <div className="commentsBody"></div>
+        <div className="commentsBody">
+            <div className="comments-header">
+                <p id='comments-heading'>comments</p>
+                <img src={comments} alt="" />
+            </div>
+        </div>
         <div className="commentsReply">
-            
+
         </div>
     </div>
   )
