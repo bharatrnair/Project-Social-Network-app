edbc93c27feeb1cf7590517e643d7136ff73e692
bharathrnair <bharathrnair7@gmail.com>
2022-05-04 16:15:34 +0530

    commit

---
+++ src/components/Save.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Save = () => {
+  return (
+    <div className="save-main-container">
+        <div className="save-body">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Save
