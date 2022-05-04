80c867d950b5980bfabcf7f8cdfc2223232c0f2e
bharathrnair <bharathrnair7@gmail.com>
2022-05-04 10:36:01 +0530

    commit

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -9,6 +9,17 @@ const Comments = () => {
                 <p id='comments-heading'>comments</p>
                 <img src={comments} alt="" />
             </div>
+            <div className="comment-section">
+                <div className="comment">
+                    <div className="comment-dp">
+
+                    </div>
+                    <div className="comment-in-detail">
+
+                    </div>
+                    <div className="comment-like-unlike" />
+                </div>
+            </div>
         </div>
         <div className="commentsReply">
 
